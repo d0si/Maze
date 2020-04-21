bad36700812cde37c3f0f8519100812300b5414f
Ziga Bobnar <ziga@bobnar.net>
2020-04-21 21:27:21 +0200

    Add helper tests for Element from_json

--- MazeTest/HelpersTest.cpp
+++ MazeTest/HelpersTest.cpp
@@ -26,7 +26,7 @@ TEST(HelpersTest, Element_ToJsonElement_Null) {
 }
 
 TEST(HelpersTest, Element_ToJsonElement_Bool) {
-	Element el = Object();
+	Element el = true;
 
 	auto result = Maze::Helpers::Element::to_json_element(&el);
 
@@ -71,6 +71,62 @@ TEST(HelpersTest, Element_ToJsonElement_Object) {
 	Element el = Object();
 
 	auto result = Maze::Helpers::Element::to_json_element(&el);
+
+	ASSERT_TRUE(result.is_object());
+}
+
+TEST(HelpersTest, Element_ElementFromJson_Null) {
+	nlohmann::json json_el;
+
+	auto result = Maze::Helpers::Element::from_json(json_el);
+
+	ASSERT_TRUE(result.is_null());
+}
+
+TEST(HelpersTest, Element_ElementFromJson_Bool) {
+	nlohmann::json json_el = true;
+
+	auto result = Maze::Helpers::Element::from_json(json_el);
+
+	ASSERT_TRUE(result.is_bool());
+}
+
+TEST(HelpersTest, Element_ElementFromJson_Int) {
+	nlohmann::json json_el = 54321;
+
+	auto result = Maze::Helpers::Element::from_json(json_el);
+
+	ASSERT_TRUE(result.is_int());
+}
+
+TEST(HelpersTest, Element_ElementFromJson_Double) {
+	nlohmann::json json_el = 9876.54321;
+
+	auto result = Maze::Helpers::Element::from_json(json_el);
+
+	ASSERT_TRUE(result.is_double());
+}
+
+TEST(HelpersTest, Element_ElementFromJson_String) {
+	nlohmann::json json_el = "test_string";
+
+	auto result = Maze::Helpers::Element::from_json(json_el);
+
+	ASSERT_TRUE(result.is_string());
+}
+
+TEST(HelpersTest, Element_ElementFromJson_Array) {
+	nlohmann::json json_el = nlohmann::json::array();
+
+	auto result = Maze::Helpers::Element::from_json(json_el);
+
+	ASSERT_TRUE(result.is_array());
+}
+
+TEST(HelpersTest, Element_ElementFromJson_Object) {
+	nlohmann::json json_el = nlohmann::json::object();
+
+	auto result = Maze::Helpers::Element::from_json(json_el);
 
 	ASSERT_TRUE(result.is_object());
 }
